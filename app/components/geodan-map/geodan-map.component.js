f85116c7667d39288b8cde12de6a42715b65fa5e
Marije Rooze <marije.rooze@geodan.nl>
2016-06-09 15:40:10 +0200

    update maps project

--- app/components/geodan-map/geodan-map.component.js
+++ app/components/geodan-map/geodan-map.component.js
@@ -3,11 +3,11 @@ import LeafletMap from './map.leaflet.js';
 var GeodanMap = {
 	templateUrl: 'components/geodan-map/geodan-map.template.html',
 	controllerAs: 'model',
-	controller: function(geodanMapService) {
+	controller: function(geodanMapService, $window, $scope) {
 		var model = this;
+		var leafletMap;
 
 		model.$onInit = function() {
-			showMap();
 			showContent();
 		}
 
@@ -18,12 +18,55 @@ var GeodanMap = {
 					model.clients = data.clients;
 					model.geodan = data.geodan[0];
 					model.activeContent = 0;
+					showMap();
 			});
 		}
 
 		function showMap() {
-			var map = new LeafletMap();
+			leafletMap = new LeafletMap();
+			leafletMap.updateMarker(model.geodan);
 		}
+
+		model.isActive = function( el ) {
+	    	return el === model.activeContent ? true : false;
+	    }
+		
+		angular.element($window).bind("scroll", function() {
+
+			var pageYOffset = this.pageYOffset,
+			  height = this.innerHeight;
+
+			var articles = document.getElementsByTagName('article');
+
+			// not perfect, need to be rewritten
+			var setActive = function(){
+			    var memo = 0;
+			    var buffer = (height * 0.2);
+
+			  for (var i = 0; i < articles.length; i++) {
+			    var el = articles[i];
+
+			    memo += el.offsetHeight;
+			    var bool = pageYOffset < (memo - buffer) ? true : false;
+
+			     if (bool) {
+			      model.activeContent = i;
+			      if (model.activeContent === 0){
+			      	 leafletMap.updateMarker(model.geodan);
+			      } else {
+			      	 leafletMap.updateMarker(model.clients[model.activeContent - 1]);
+			      }
+			     
+			      break;
+			    }
+			  }
+
+			}
+
+			setActive();
+
+			$scope.$apply();
+        });
 
 		
 	}
--- app/components/geodan-map/map.leaflet.js
+++ app/components/geodan-map/map.leaflet.js
@@ -2,31 +2,18 @@ import L from 'Leaflet';
 
 function LeafletMap() {
 
-  var zoomlevel = 12;
-  var markersLayer = new L.LayerGroup();
+  var zoomlevel = 9,
+      markersLayer = new L.LayerGroup(),
+      markers = [];
 
-   var map = L.map('map', {
+   var map = L.map('mapLeaflet', {
    		center: new L.LatLng(52.341758, 4.911880),
-   		zoom: 7,
+   		zoom: zoomlevel,
    		zoomControl: false
     });
 
-
-  var testIcon = L.icon({
-      iconUrl: 'img/plus_sign.gif',
-     // shadowUrl: 'leaf-shadow.png',
-
-      iconSize:     [37, 33], // size of the icon
-     // shadowSize:   [50, 64], // size of the shadow
-      iconAnchor:   [18, 16], // point of the icon which will correspond to marker's location
-    //  shadowAnchor: [4, 62],  // the same for the shadow
-      popupAnchor:  [-3, -18] // point from which the popup should open relative to the iconAnchor
-  });
-
-
-
-  var testIcon2 = L.icon({
-      iconUrl: 'img/marker_test_02.png',
+  var customIcon = L.icon({
+      iconUrl: 'components/geodan-map/assets/marker_test_02.gif',
      // shadowUrl: 'leaf-shadow.png',
 
       iconSize:     [30, 33], // size of the icon
@@ -36,10 +23,8 @@ function LeafletMap() {
       popupAnchor:  [-3, -18] // point from which the popup should open relative to the iconAnchor
   });
 
-
-
   	// add an OpenStreetMap tile layer
-  	L.tileLayer('http://tile.stamen.com/toner-lite/{z}/{x}/{y}.png', {
+  L.tileLayer('http://tile.stamen.com/toner-lite/{z}/{x}/{y}.png', {
   		attribution: '&copy; <a href="http://osm.org/copyright">Stamen</a> contributors',
       opacity: 0.5,
       detectRetina: true,
@@ -48,7 +33,7 @@ function LeafletMap() {
   	}).addTo(map);
 
    // remove all map zooms etc
-   var disableMapEvents = function(){
+   var disableMapEvents = function () {
       map.dragging.disable();
       map.touchZoom.disable();
       map.doubleClickZoom.disable();
@@ -56,19 +41,19 @@ function LeafletMap() {
       if (map.tap) map.tap.disable();
    }
 
-    // scope.$watch('activeContent', function( newValue, oldValue ){
-    //     if( !newValue && newValue === oldValue ) return;
+   function removePreviousMarkers() {
+      for(var i = 0; i < markers.length; i++) {
+        map.removeLayer(markers[i]);
+      }
+   }
 
-    //     var obj = scope.clients[scope.activeContent - 1];
-    //         obj = scope.activeContent - 1 < 0 ? scope.geodan : obj;
-        
-    //     if( markersLayer ) markersLayer.clearLayers(); //map.removeLayer( markersLayer );
-    //     updateView( obj );
-    //     setMarkers( obj.coord, obj.name );
-      
-    // });
+    this.updateMarker = function (obj) {
+      removePreviousMarkers();
+        updateView( obj );
+        setMarkers( obj.coord, obj.name );
+    }
 
-    var updateView = function( obj ){
+    var updateView = function (obj) {
       var latlng = new L.LatLng( obj.coord[0].lat, obj.coord[0].long );
     
       var offset = map._getNewTopLeftPoint(latlng).subtract(map._getTopLeftPoint());
@@ -76,17 +61,18 @@ function LeafletMap() {
 
     }
    
-    var setMarkers = function( coord, name ){
+    var setMarkers = function (coord, name) {
       var popupContent = name;
+      markers = [];
       for( var i = 0; i < coord.length; i++){
-         // var marker = L.marker([coord[i].lat, coord[i].long], {icon: testIcon2})
-         //               .bindPopup( popupContent );
 
-          var marker = L.marker([coord[i].lat, coord[i].long])
+          var marker = L.marker([coord[i].lat, coord[i].long], {icon: customIcon})
                         .bindPopup( popupContent );
 
 
           markersLayer.addLayer(marker); 
+
+          markers.push(marker);
       }
 
       markersLayer.addTo(map);
